4dbe437b59ab1998aabc6eb7becd597a5cdf09c5
Leon Wabeke <leonwabeke@gmail.com>
2018-07-09 17:03:22 +0200

    Initial changes to try to support version 0.7 without any warnings (#13)
    * Initial changes to try to support version 0.7 without any warnings
    
    * Fixed one more deprecation warning and updated travis and appveyor to remove 0.6
    
    * Another change to appveyor paths

--- src/Unmarshal.jl
+++ src/Unmarshal.jl
@@ -47,7 +47,7 @@ function unmarshal(::Type{Array{E, N}}, parsedJson::Vector, verbose :: Bool = fa
         verboseLvl+=1
     end
 
-    cat(N, (unmarshal(Array{E,N-1}, x, verbose, verboseLvl) for x in parsedJson)...)
+    cat((unmarshal(Array{E,N-1}, x, verbose, verboseLvl) for x in parsedJson)..., dims=N)
 end
 
 function unmarshal(::Type{Vector{E}}, parsedJson::Number, verbose :: Bool = false, verboseLvl :: Int = 0) where E<:Number
@@ -67,7 +67,7 @@ function unmarshal(::Type{Array{E, N}}, parsedJson::Number, verbose :: Bool = fa
         verboseLvl+=1
     end
 
-    cat(N, E(parsedJson))
+    cat(E(parsedJson), dims=N)
 end
 
 
@@ -90,20 +90,20 @@ true
 ```
 
 """
-function unmarshal(DT :: Type, parsedJson :: Associative, verbose :: Bool = false, verboseLvl :: Int = 0)
+function unmarshal(DT :: Type, parsedJson :: AbstractDict, verbose :: Bool = false, verboseLvl :: Int = 0)
     if (verbose)
-            prettyPrint(verboseLvl, "$(DT) Associative")
+            prettyPrint(verboseLvl, "$(DT) AbstractDict")
         verboseLvl+=1
     end
 
-    if !isleaftype(DT)
+    if !isconcretetype(DT)
         throw(ArgumentError("Cannot unmarshal a non-leaf type $(DT) without a custom specialization"))
     end
 
     tup = ()
     for iter in fieldnames(DT)
         DTNext = fieldtype(DT,iter)
-#        @show iter, DTNext, !haskey(parsedJson, string(iter))
+ #       @show iter, DTNext, !haskey(parsedJson, string(iter))
 
         if !haskey(parsedJson, string(iter)) 
             # check whether DTNext is compatible with any scheme for missing values
@@ -111,10 +111,13 @@ function unmarshal(DT :: Type, parsedJson :: Associative, verbose :: Bool = fals
                 DTNext()
             elseif Missing <: DTNext
                 missing
+            elseif Nothing <: DTNext
+                Nothing()
             else
-                throw(ArgumentError("Key $(string(iter)) is missing from the structure $DT, and field is neither Nullable nor Missings-compatible"))
+                throw(ArgumentError("Key $(string(iter)) is missing from the structure $DT, and field is neither Nullable nor Missings nor Nothing-compatible"))
             end
         else
+ #@show DTNext, parsedJson[string(iter)], verbose, verboseLvl
             val = unmarshal( DTNext, parsedJson[string(iter)], verbose, verboseLvl)
         end
 
@@ -133,7 +136,7 @@ function unmarshal(DT :: Type{T}, parsedJson :: Array{Any,N}, verbose :: Bool = 
     ((unmarshal(fieldtype(T,1), field, verbose, verboseLvl) for field in parsedJson)...,)
 end
 
-function unmarshal(DT :: Type{T}, parsedJson :: Associative, verbose :: Bool = false, verboseLvl :: Int = 0) where T <: Dict
+function unmarshal(DT :: Type{T}, parsedJson :: AbstractDict, verbose :: Bool = false, verboseLvl :: Int = 0) where T <: Dict
     if (verbose)
         prettyPrint(verboseLvl, "$(DT) Dict ")
         verboseLvl += 1
@@ -147,8 +150,9 @@ end
 
 unmarshal(::Type{T}, x::Number, verbose :: Bool = false, verboseLvl :: Int = 0) where T<:Number = T(x)
 unmarshal(::Type{Nullable{T}}, x, verbose :: Bool = false, verboseLvl :: Int = 0) where T = Nullable(unmarshal(T, x))
-unmarshal(::Type{Nullable{T}}, x::Void, verbose :: Bool = false, verboseLvl :: Int = 0) where T = Nullable{T}()
+unmarshal(::Type{Nullable{T}}, x::Nothing, verbose :: Bool = false, verboseLvl :: Int = 0) where T = Nullable{T}()
 unmarshal(::Type{Union{T,Missing}}, x, verbose :: Bool = false, verboseLvl :: Int = 0) where T = unmarshal(T, x, verbose, verboseLvl)
+unmarshal(::Type{Union{T,Nothing}}, x, verbose :: Bool = false, verboseLvl :: Int = 0) where T = unmarshal(T, x, verbose, verboseLvl)
 
 unmarshal(T::Type, x, verbose :: Bool = false, verboseLvl :: Int = 0) =
     throw(ArgumentError("no unmarshal function defined to convert $(typeof(x)) to $(T); consider providing a specialization"))
