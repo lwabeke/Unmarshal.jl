98e3cd429f2685d9857c961b03d8102fc3400d7b
Leon Wabeke <leonwabeke@gmail.com>
2020-04-17 14:44:30 +0200

    Updates to documentation and README

--- src/Unmarshal.jl
+++ src/Unmarshal.jl
@@ -70,7 +70,7 @@ unmarshal(::Type{Array}, xs::Union{Vector,AbstractArray}, verbose :: Bool = fals
 """
     unmarshal(T, dict[, verbose[, verboselvl]])
 
-Reconstructs an object of Type T using the dictionary output of a `JSON.parse.`
+Reconstructs an object of Type T using the dictionary output of a `JSON.parse` or now 'LazyJSON.parse`.
 
 Set verbose `true` to get debug information about how the data hierarchy is unmarshalled. This might be useful to track down parsing errors and/or mismatches between the JSON object and the Type definition.
 
@@ -82,6 +82,15 @@ julia> using JSON
 julia> var = randn(Float64, 5);  # Should work for most other variations of types you can think of
 
 julia> unmarshal(typeof(var), JSON.parse(JSON.json(var)) ) == var
+true
+ 
+or 
+
+julia> using LazyJSON
+
+julia> var = randn(Float64, 5);  # Should work for most other variations of types you can think of
+
+julia> unmarshal(typeof(var), LazyJSON.parse(JSON.json(var)) ) == var
 true
 ```
 
